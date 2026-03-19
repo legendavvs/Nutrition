8d95cdfa087fdeb5a09bae6bd2bbbb507c899605
Legendavvs <tgsktgam12341234@gmail.com>
2026-03-19 18:48:17 +0200

    fix: update search to use prefix/suffix wildcards

--- api/search.js
+++ api/search.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const searchUrl = `https://world.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=search_terms&tag_contains_0=contains&tag_0=${encodeURIComponent(q)}&sort_by=unique_scans_n&page_size=40&json=1`
+    const searchUrl = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=*${encodeURIComponent(q)}*&search_simple=1&action=process&json=1&page_size=30`
     const response = await fetch(
       searchUrl,
       {
